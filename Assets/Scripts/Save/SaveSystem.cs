3e3f1a42f1a245a0a00de75372b137d308250f24
alexisnemsingh <alexisnemsingh@gmail.com>
2025-01-29 18:21:55 -0800

    basic save system file (will add more once player is created)

--- Assets/Scripts/Save/SaveSystem.cs
+++ Assets/Scripts/Save/SaveSystem.cs
@@ -5,11 +5,20 @@ using System.IO;
 
 public class SaveSystem
 {
+    private static SaveData _saveData = new SaveData();
+    
     [System.Serializable]
     public struct SaveData
     {
-        
+        // public PlayerSaveData PlayerData;
+        // PlayerSaveData needs to be made in Player.cs
     }
-    
+
+    // creates save file
+    public static string SaveFilePath()
+    {
+        string saveFile = Application.persistentDataPath + "/save" + ".save";
+        return saveFile;
+    }
     
 }
