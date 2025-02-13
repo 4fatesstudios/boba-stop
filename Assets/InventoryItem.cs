d7aadffc10727a2cfab3141411fc1d84e6603e04
alexisnemsingh <alexisnemsingh@gmail.com>
2025-02-12 23:42:43 -0800

    added InventoryItem.cs

---
+++ Assets/InventoryItem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("begin drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("dragging");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("end drag");
+    }
+}
