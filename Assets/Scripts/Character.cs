4b3319c74dfbe827de622b0d9825d1e44144c430
Christopher Liwanag <cliwanag@csu.fullerton.edu>
2025-02-19 10:21:20 -0800

    removed using Unity.UI

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using SimpleCombat;
 using SimpleCombat.Components;
-using UnityEditor.UI;
 using UnityEngine;
 
 public abstract class Character : MonoBehaviour, IDamageable {
