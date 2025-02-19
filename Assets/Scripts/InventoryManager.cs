6981278b2bff763f87ee1d681c88c76f158c0d6d
alexisnemsingh <alexisnemsingh@gmail.com>
2025-02-18 17:31:25 -0800

    implemented additem logic

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,10 +4,21 @@ public class InventoryManager : MonoBehaviour
 {
     public InventorySlot weaponSlot;
     public InventorySlot armorSlot;
+    
+    public InventorySlot[] inventorySlots;
 
     public void AddItem(Item item)
     {
-        
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(item, slot);
+                return;
+            }
+        }
     }
 
     void SpawnNewItem(Item item, InventorySlot slot)
