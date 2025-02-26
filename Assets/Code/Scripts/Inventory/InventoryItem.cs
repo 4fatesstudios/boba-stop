ff0b946d6d62583d9597f003e099fab9b34906c7
alexisnemsingh <alexisnemsingh@gmail.com>
2025-02-25 22:22:11 -0800

    restored drag and drop in inventory

--- Assets/Code/Scripts/Inventory/InventoryItem.cs
+++ Assets/Code/Scripts/Inventory/InventoryItem.cs
@@ -1,31 +1,32 @@
-// using UnityEngine;
-// using UnityEngine.UI;
-// using UnityEngine.EventSystems;
-//
-// namespace BobaStop.Inventory {
-//     public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
-//
-//         public Item item;
-//         public Image image;
-//         [HideInInspector] public Transform parentAfterDrag;
-//
-//         public void OnBeginDrag(PointerEventData eventData) {
-//             Debug.Log("begin drag");
-//             parentAfterDrag = transform.parent;
-//             transform.SetParent(transform.root);
-//             transform.SetAsLastSibling();
-//             image.raycastTarget = false;
-//         }
-//
-//         public void OnDrag(PointerEventData eventData) {
-//             Debug.Log("dragging");
-//             transform.position = Input.mousePosition;
-//         }
-//
-//         public void OnEndDrag(PointerEventData eventData) {
-//             Debug.Log("end drag");
-//             transform.SetParent(parentAfterDrag);
-//             image.raycastTarget = true;
-//         }
-//     }
-// }
+using BobaStop.Items;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace BobaStop.Inventory {
+    public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+        public Item item;
+        public Image image;
+        [HideInInspector] public Transform parentAfterDrag;
+
+        public void OnBeginDrag(PointerEventData eventData) {
+            Debug.Log("begin drag");
+            parentAfterDrag = transform.parent;
+            transform.SetParent(transform.root);
+            transform.SetAsLastSibling();
+            image.raycastTarget = false;
+        }
+
+        public void OnDrag(PointerEventData eventData) {
+            Debug.Log("dragging");
+            transform.position = Input.mousePosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData) {
+            Debug.Log("end drag");
+            transform.SetParent(parentAfterDrag);
+            image.raycastTarget = true;
+        }
+    }
+}
--- Assets/Code/Scripts/Inventory/InventorySlot.cs
+++ Assets/Code/Scripts/Inventory/InventorySlot.cs
@@ -1,16 +1,43 @@
-// using UnityEngine;
-// using UnityEngine.EventSystems;
-//
-// namespace BobaStop.Inventory {
-//     public class InventorySlot : MonoBehaviour, IDropHandler {
-//         public ItemType allowedItemType;
-//
-//         public void OnDrop(PointerEventData eventData) {
-//             if (transform.childCount == 0) {
-//                 GameObject dropped = eventData.pointerDrag;
-//                 InventoryItem inventoryItem = dropped.GetComponent<InventoryItem>();
-//                 inventoryItem.parentAfterDrag = transform;
-//             }
-//         }
-//     }
-// }
+using BobaStop.Items;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace BobaStop.Inventory
+{
+    public class InventorySlot : MonoBehaviour, IDropHandler
+    {
+        public bool allowResources = true; // Allow Resource items
+        public bool allowWeapons = true;   // Allow Weapon items
+        public bool allowPacks = true;    // Allow Pack items
+        public bool allowGear = true;     // Allow Gear items
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (transform.childCount == 0) // Check if the slot is empty
+            {
+                GameObject dropped = eventData.pointerDrag;
+                InventoryItem inventoryItem = dropped.GetComponent<InventoryItem>();
+
+                // Check if the item is allowed in this slot
+                if (IsItemAllowed(inventoryItem.item))
+                {
+                    inventoryItem.parentAfterDrag = transform;
+                }
+                else
+                {
+                    Debug.Log("This item is not allowed in this slot.");
+                }
+            }
+        }
+
+        // Helper method to check if the item is allowed
+        private bool IsItemAllowed(Item item)
+        {
+            if (item is Resource && allowResources) return true;
+            if (item is Weapon && allowWeapons) return true;
+            if (item is Pack && allowPacks) return true;
+            if (item is Gear && allowGear) return true;
+            return false;
+        }
+    }
+}
