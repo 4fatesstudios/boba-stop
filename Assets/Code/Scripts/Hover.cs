f669ab76e8e57c108f2f530729cf4797eb4769cb
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-02-25 21:14:13 -0800

    added random offset to float

--- Assets/Code/Scripts/Hover.cs
+++ Assets/Code/Scripts/Hover.cs
@@ -6,17 +6,18 @@ namespace BobaStop
 {
     public class Hover : MonoBehaviour
     {
-        public float hoverHeight = 0.05f;
-        public float hoverSpeed = 2.5f;
-    
+        [SerializeField] private float hoverHeight = 0.05f;
+        [SerializeField] private float hoverSpeed = 2.5f;
+        private float randomOffset;
         private Vector3 startPos;
 
         void Start() {
+            randomOffset = Random.value;
             startPos = transform.localPosition;
         }
 
         void Update() {
-            transform.localPosition = startPos + Mathf.Sin(Time.time * hoverSpeed) * hoverHeight * Vector3.up;
+            transform.localPosition = startPos + Mathf.Sin(Time.time + randomOffset * hoverSpeed) * hoverHeight * Vector3.up;
         }
     }
 }
