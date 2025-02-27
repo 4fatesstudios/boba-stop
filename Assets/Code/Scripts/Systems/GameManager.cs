2f7a4ee8011b4bc0713d7ee89cf19370134818d5
Christopher Liwanag <cliwanag@csu.fullerton.edu>
2025-02-26 21:00:52 -0800

    organization

--- Assets/Code/Scripts/Systems/GameManager.cs
+++ Assets/Code/Scripts/Systems/GameManager.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace BobaStop.Systems {
     public class GameManager : MonoBehaviour {
         public static GameManager Instance { get; private set; }
         
+        #region Manager Instances
         private static SaveSystem saveSystem;
         private static PlayerDataManager playerDataManager;
         private static WorldDataManager worldDataManager;
         private static DayCycleManager dayCycleManager;
         private static int saveSlot;
+        #endregion
         
-
+        #region Unity Functions
         private void Awake() {
             if (Instance == null) {
                 Instance = this;
@@ -37,7 +38,9 @@ namespace BobaStop.Systems {
         private void Update() {
             dayCycleManager.Update();
         }
-
+        #endregion
+        
+        #region SaveSystem Functions
         public void SetSaveSlot(int slot) {
             saveSlot = slot;
         }
@@ -51,7 +54,9 @@ namespace BobaStop.Systems {
             saveSystem.SetSaveLoadSlot(saveSlot);
             saveSystem.SaveData(playerDataManager.playerData, worldDataManager.worldData);
         }
-
+        #endregion
+        
+        #region Player Data Functions
         public bool SetPlayerName(string playerName) {
             return playerDataManager.SetPlayerName(playerName);
         }
@@ -67,7 +72,9 @@ namespace BobaStop.Systems {
         public void AddPlayerPearls(int pearls) {
             playerDataManager.AddPlayerPearls(pearls);
         }
-
+        #endregion
+        
+        #region Day Cycle Functions
         public string GetStandardTime() {
             return dayCycleManager.GetStandardTime();
         }
@@ -75,5 +82,18 @@ namespace BobaStop.Systems {
         public string GetMilitaryTime() {
             return dayCycleManager.GetMilitaryTime();
         }
+        
+        public void SubscribeToDayPhaseChanges(EventHandler<DayCycleManager.OnDayPhaseChangeEventArgs> handler)
+        {
+            dayCycleManager.OnDayPhaseChanged += handler;
+        }
+
+        public void UnsubscribeFromDayPhaseChanges(EventHandler<DayCycleManager.OnDayPhaseChangeEventArgs> handler)
+        {
+            dayCycleManager.OnDayPhaseChanged -= handler;
+        }
+        #endregion
+        
+        
     }
 }
