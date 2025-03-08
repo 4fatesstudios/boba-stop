5f48cf10233e9da54d18c31b9dd1b5aed4e9fb62
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-03-08 01:51:36 -0800

    comment removals

--- Assets/Code/Scripts/Characters/Animation/EnemyKnifeAnimator.cs
+++ Assets/Code/Scripts/Characters/Animation/EnemyKnifeAnimator.cs
@@ -7,13 +7,11 @@ namespace BobaStop.Characters.Animation {
     public class EnemyKnifeAnimator : CharacterAnimator {
         // call from specific associated animation frame event
         public override void OnAttackAnimation_Attack() {
-            Debug.Log("enemy attack called");
             character.OnAttack();
         }
 
         // call from specific associated animation frame event
         public override void OnAttackAnimation_Finish() {
-            Debug.Log("enemy animation finish called");
             character.OnAttackFinish();
         }
     }
