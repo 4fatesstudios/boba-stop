ce707f809edff1354341098e638be866c01d0075
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-03-18 01:23:40 -0700

    schedule improvements

--- Assets/Code/Scripts/Systems/ScheduleManager.cs
+++ Assets/Code/Scripts/Systems/ScheduleManager.cs
@@ -1,7 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using BobaStop.Data.Saved;
-using UnityEngine;
+using BobaStop.Systems.World;
+using UnityEngine.Events;
 
 namespace BobaStop.Systems
 {
@@ -10,11 +10,26 @@ namespace BobaStop.Systems
         private float startTime;
         private float endTime;
 
+        public ScheduledTime(ScheduledTime scheduledTime) {
+            startTime = scheduledTime.GetStartTime();
+            endTime = scheduledTime.GetEndTime();
+            
+            ValidateScheduledTime();
+        }
+        
         public ScheduledTime(float startTime, float endTime) {
             this.startTime = startTime;
             this.endTime = endTime;
 
             ValidateScheduledTime();
+        }
+
+        public float GetStartTime() {
+            return startTime;
+        }
+
+        public float GetEndTime() {
+            return endTime;
         }
 
         public void ChangeStartTime(float newStartTime) {
@@ -45,8 +60,7 @@ namespace BobaStop.Systems
     }
     
     // struct that takes timeofdays list and set times
-    public class Schedule
-    {
+    public class Schedule {
         private Dictionary<DayOfWeek, ScheduledTime> operations = new() {
             { DayOfWeek.Sunday, new ScheduledTime() },
             { DayOfWeek.Monday, new ScheduledTime() },
@@ -65,9 +79,66 @@ namespace BobaStop.Systems
             return operations[dayOfWeek];
         }
     }
+
+    public interface IScheduledObject {
+        public ScheduledTime GetScheduledTime();
+        public void SetIsScheduledTime(bool isScheduledTime);
+        public bool GetIsScheduledTime();
+    }
+
+    public struct ScheduleData {
+        public IScheduledObject scheduledObject;
+        public ScheduledTime scheduledTime;
+
+        public ScheduleData(IScheduledObject scheduledObject, ScheduledTime scheduledTime) {
+            this.scheduledObject = scheduledObject;
+            this.scheduledTime = scheduledTime;
+        }
+    }
     
-    public class ScheduleManager : MonoBehaviour {
-        // subscribe to 
+    public class ScheduleManager : RealtimeSystemManager {
+        List<ScheduleData> scheduledObjects = new();
+        private float currentTime;
         
+        public override void Start() {
+            
+        }
+        
+        public override void Update() {
+            currentTime = GameManager.Instance.dayCycleManager.GetAdjustedTime();
+            CheckScheduledObjects();
+        }
+
+        public void AddToSchedule(IScheduledObject scheduledObject, ScheduledTime scheduledTime) {
+            scheduledObjects.Add(new ScheduleData(scheduledObject, scheduledTime));
+        }
+
+        private void CheckScheduledObjects() {
+            foreach (var scheduledObject in scheduledObjects) {
+                HandleScheduledData(scheduledObject);
+            }
+        }
+
+        private void HandleScheduledData(ScheduleData scheduleData) {
+            var scheduledTime = scheduleData.scheduledTime;
+            var scheduledObject = scheduleData.scheduledObject;
+
+            // return early if not scheduled
+            if (scheduledObject.GetIsScheduledTime() == false &&
+                !IsFloatInbetween(scheduledTime.GetStartTime(), scheduledTime.GetEndTime(), currentTime)) return;
+
+            // return early if the scheduled time is not valid for setting it to true
+            if (scheduledTime.GetStartTime() > currentTime || scheduledTime.GetEndTime() < currentTime) {
+                scheduledObject.SetIsScheduledTime(false);
+                return;
+            }
+
+            // if between the times, set it to true
+            scheduledObject.SetIsScheduledTime(true);
+        }
+
+        private bool IsFloatInbetween(float a, float b, float t) {
+            return t <= a && t >= b;
+        }
     }
 }
