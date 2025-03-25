082be02be3c47ef8c28af7b859df8527d608c7b0
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-03-24 19:56:12 -0700

    npc dialogue data

---
+++ Assets/ScriptableObjects/Data/NPCs/NPCDialogueData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BobaStop.NPCs
+{
+    [System.Serializable]
+    public class NPCDialogueData : ScriptableObject {
+        
+    }
+}
